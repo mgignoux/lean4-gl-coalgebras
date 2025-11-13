e183049bc269103423c79ba965eeb53121a1591c
Madeleine Gignoux <madeleine.gignoux@gmail.com>
2025-11-13 18:57:45 +0100

    Update root

--- GL.lean
+++ GL.lean
@@ -3,6 +3,8 @@ import GL.CoalgebraProof
 import GL.SplitCoalgebraProof
 import GL.Interpolants
 import GL.PartialInterpolation
+import GL.Semantics
+import GL.Soundness
 
 -- This module serves as the root of the `MasterThesis` library.
 -- Import modules here that should be built as part of the library.
